dfcd09177c69289e5ae28a07612f617ed796317a
pedro-natal <pedropferreiranatal@gmail.com>
2024-06-03 21:01:00 -0300

    atualizações no projeto

--- 01 - Recapitulação de Lógica de Programação usando C#/Exercícios/Saídas de Dados em C#/obj/Debug/net8.0/Saídas de Dados em C#.AssemblyInfo.cs
+++ 01 - Recapitulação de Lógica de Programação usando C#/Exercícios/Saídas de Dados em C#/obj/Debug/net8.0/Saídas de Dados em C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Saídas de Dados em C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfdfc566d60ec8879ad35f0ab139c4b839811724")]
 [assembly: System.Reflection.AssemblyProductAttribute("Saídas de Dados em C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Saídas de Dados em C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
