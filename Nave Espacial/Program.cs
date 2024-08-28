68814ec79236835d917de43ea1491dd2760de9d5
pedro-natal <pedropferreiranatal@gmail.com>
2024-08-28 10:45:04 -0300

    adicionado novo projeto

---
+++ Nave Espacial/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Bem-vindo ao nosso sistema!");
+Console.WriteLine("Para começar, digite o número do setor que deseja acessar:");
+Console.WriteLine("1 - Tripulantes");
+Console.WriteLine("2 - Destinos");
+int menu = int.Parse(Console.ReadLine());
+
+if (menu == 1)
+{
+    Console.WriteLine("1- Cadastrar Tripulantes");
+    Console.WriteLine("2- Listar Tripulantes");
+    Console.WriteLine("3- Deletar Tripulantes");
+}
+else
+{
+    Console.WriteLine("1- Cadastrar Destinos");
+    Console.WriteLine("2- Listar Destinos");
+    Console.WriteLine("3- Deletar Destinos");
+}
