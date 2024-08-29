7a46b44254de1cc1c5f00b01b0da7df1b945a691
luizpdbt <luiz.uapiba@hotmail.com>
2024-08-28 22:20:20 -0300

    Update Program.cs
    Correções e ajustes

--- Nave Espacial/Program.cs
+++ Nave Espacial/Program.cs
@@ -6,12 +6,18 @@ int menu = int.Parse(Console.ReadLine());
 
 if (menu == 1)
 {
+    //Cade a leitura para cada menu desses tb? Caso ele escolha o cadastro de tripulantes como vc vai saber?
+    
     Console.WriteLine("1- Cadastrar Tripulantes");
     Console.WriteLine("2- Listar Tripulantes");
+
+    //Caso seja escolhido deletar, vc tera que pedir o codigo do tripulante correto?
     Console.WriteLine("3- Deletar Tripulantes");
 }
 else
 {
+    //Cade a leitura para cada menu desses tb? Caso ele escolha o cadastro de destinos como vc vai saber?
+    
     Console.WriteLine("1- Cadastrar Destinos");
     Console.WriteLine("2- Listar Destinos");
     Console.WriteLine("3- Deletar Destinos");
