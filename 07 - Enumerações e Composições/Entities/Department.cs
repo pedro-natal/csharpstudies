137cff269e1486428177266b861606e472d86b38
pedro-natal <pedropferreiranatal@gmail.com>
2024-09-05 15:25:53 -0300

    atualizações no projeto

---
+++ 07 - Enumerações e Composições/Entities/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace _07___Enumerações_e_Composições.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ 07 - Enumerações e Composições/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace _07___Enumerações_e_Composições.Entities.Enums
+{
+    internal enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
