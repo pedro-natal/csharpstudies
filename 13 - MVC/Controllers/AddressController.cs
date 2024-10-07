33d68a47efdadc24157295c30ec2037e51fd4a57
pedro-natal <pedropferreiranatal@gmail.com>
2024-10-07 09:52:12 -0300

    atualizações no projeto

---
+++ 13 - MVC/Controllers/AddressController.cs
@@ -0,0 +1,49 @@
+﻿using _13___MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _13___MVC.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: AddressController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Addresss/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Address/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AddressModel data)
+        {
+            if (ModelState.IsValid == false)
+            {
+                {
+                    _logger.LogWarning("The user submitted an invalid Address upon Create.");
+                    return View();
+                }
+            }
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 13 - MVC/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _13___MVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: PersonController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PersonController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
