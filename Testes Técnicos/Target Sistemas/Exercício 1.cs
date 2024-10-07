c2d6d9b6b7421c8784dd7dda56dfaa05a56d63bc
pedro-natal <pedropferreiranatal@gmail.com>
2024-10-07 17:56:51 -0300

    atualizações no projeto

---
+++ Testes Técnicos/Target Sistemas/Exercício 1.cs
@@ -0,0 +1,29 @@
+﻿class Exercicio1
+{
+    static bool ChecarFibonacci(int num)
+    {
+        int a = 0, b = 1;
+        while (b < num)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return b == num || num == 0;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Informe um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (ChecarFibonacci(numero))
+        {
+            Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} não pertence à sequência de Fibonacci.");
+        }
+    }
+}
---
+++ Testes Técnicos/Target Sistemas/Exercício 2.cs
@@ -0,0 +1,31 @@
+﻿class Exercicio2
+{
+    static int ContaOcorrencias(string str)
+    {
+        int count = 0;
+        foreach (char c in str.ToLower())
+        {
+            if (c == 'a')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Informe uma string: ");
+        string input = Console.ReadLine();
+
+        int count = ContaOcorrencias(input);
+        if (count > 0)
+        {
+            Console.WriteLine($"A letra 'a' ocorre {count} vezes na string informada.");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' não ocorre na string informada.");
+        }
+    }
+}
